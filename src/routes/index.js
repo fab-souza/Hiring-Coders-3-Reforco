2323aed8cfce8b62745e165f003bf34e19d3db84
Fab de Souza <fs_7059@yahoo.com>
2022-06-28 08:48:59 -0300

    aula 38

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const produtoController = require('../controllers/produtoController');
 const routes = express.Router()
 
 routes.get("/produto/lista", produtoController.listarProduto);
-
+routes.post("/produto", produtoController.cadastrarProduto);
 
 
 module.exports = routes;
